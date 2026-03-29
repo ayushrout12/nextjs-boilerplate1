ca6db809a68b166eb577141f006aceda25ae7b77
ayushrout12 <100101036+ayushrout12@users.noreply.github.com>
2026-03-28 22:37:23 -0700

    Replace Lotus logo image with flower emoji

--- app/components/LotusLogo.tsx
+++ app/components/LotusLogo.tsx
@@ -1,19 +1,12 @@
-import Image from "next/image";
-
 export default function LotusLogo() {
   return (
-    <div style={{ marginBottom: "20px" }}>
-      <Image
-        src="/lotus.png"
-        alt="Lotus Logo"
-        width={80}
-        height={80}
-        priority
-        style={{
-          display: "block",
-          margin: "0 auto"
-        }}
-      />
+    <div
+      style={{
+        fontSize: "64px",
+        marginBottom: "20px"
+      }}
+    >
+      🌸
     </div>
   );
 }
