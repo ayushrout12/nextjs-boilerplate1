cd580502eceb6e92019881b1494dd322d70acc57
ayushrout12 <100101036+ayushrout12@users.noreply.github.com>
2026-03-28 22:36:02 -0700

    Update LotusLogo.tsx

--- app/components/LotusLogo.tsx
+++ app/components/LotusLogo.tsx
@@ -1,13 +1,19 @@
+import Image from "next/image";
+
 export default function LotusLogo() {
   return (
-    <div
-      style={{
-        fontSize: "36px",
-        marginBottom: "12px",
-        animation: "float 4s ease-in-out infinite"
-      }}
-    >
-      🌸
+    <div style={{ marginBottom: "20px" }}>
+      <Image
+        src="/lotus.png"
+        alt="Lotus Logo"
+        width={80}
+        height={80}
+        priority
+        style={{
+          display: "block",
+          margin: "0 auto"
+        }}
+      />
     </div>
-  )
+  );
 }
